1d824d97605c8fc121f3668a7f24804315f8256a
Ivan <ivanaires32@gmail.com>
2023-09-25 12:31:27 -0300

    fix: server port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  const port = process.env.PORT || 3000
+  await app.listen(port, () => console.log(`server running on port ${port}`));
 }
 bootstrap();
